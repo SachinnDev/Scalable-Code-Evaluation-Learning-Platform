f641d4a0c2e0525be4bec1c23f2cfa1ad808fd95
SachinnDev <sachinkumarojhaji@gmail.com>
2026-04-19 14:31:40 +0530

    Removed unnecessary Day01-Day08 folders

--- Day07/src/routes/submit.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const submitRouter= express.Router();
-const userMiddleware=require("../middleware/userMiddleware");
-constCode=require("../controllers/userSubmission")
-
-submitRouter.post("/submit/:id",userMiddleware,submitCode);
